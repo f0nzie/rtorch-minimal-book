341d7696053c6ca5bfdbb72f09e03822d8d441e5
og-analytics <og-analytics@oilgainsanalytics.com>
2020-10-14 07:53:44 -0500

    indent; add comments; delete blank lines

--- js/codefolding.js
+++ js/codefolding.js
@@ -13,7 +13,7 @@ window.initializeCodeFolding = function(show) {
         $(this).collapse('show');
       });    
   });
-    $("#rmd-hide-all-code").click(function() {
+  $("#rmd-hide-all-code").click(function() {
       // close the dropdown menu when an option is clicked
       $("#allCodeButton").dropdown("toggle");
       // Hide R code
@@ -27,25 +27,24 @@ window.initializeCodeFolding = function(show) {
   });
 
   // index for unique code element ids
-  var r_currentIndex  = 1;
-  var py_currentIndex = 1;
+  var r_currentIndex  = 1;   // for R code
+  var py_currentIndex = 1;   // for Python code
 
-  
   // select Python chunks
   var pyCodeBlocks = $('pre.python');
   pyCodeBlocks.each(function() {
-    
     // create a collapsable div to wrap the code in
     var div = $('<div class="collapse py-code-collapse"></div>');
     if (show)
       div.addClass('in');
     var id = 'pycode-643E0F36' + py_currentIndex++;
     div.attr('id', id);
+    // "this" refers the code chunk
     $(this).before(div);
     $(this).detach().appendTo(div);
-    $(this).css('background-color','#ebfaeb');
+    $(this).css('background-color','#ebfaeb');  // change color of chunk background
     
-  // add a show code button right above
+    // add a show code button right above
     var showCodeText = $('<span>' + (show ? 'Hide Python code' : 'Python code') + '</span>');
     var showCodeButton = $('<button type="button" class="btn btn-default btn-xs code-folding-btn pull-right"></button>');
     showCodeButton.append(showCodeText);
@@ -55,7 +54,7 @@ window.initializeCodeFolding = function(show) {
         .attr('aria-expanded', show)
         .attr('aria-controls', id);    
         
-    // showCodeButton.css('background-color','#f47121');
+    // change the background color of the button
     showCodeButton.css('background-color','#009900');
         
     var buttonRow = $('<div class="row"></div>');
@@ -81,7 +80,6 @@ window.initializeCodeFolding = function(show) {
   // adding pre.sourceCode confuses the Python button
   var rCodeBlocks = $('pre.r, pre.bash, pre.sql, pre.cpp, pre.stan');
   rCodeBlocks.each(function() {
-
     // create a collapsable div to wrap the code in
     var div = $('<div class="collapse r-code-collapse"></div>');
     if (show)
@@ -90,7 +88,7 @@ window.initializeCodeFolding = function(show) {
     div.attr('id', id);
     $(this).before(div);
     $(this).detach().appendTo(div);
-    $(this).css('background-color','#e6faff');
+    $(this).css('background-color','#e6faff'); // change color of chunk background
 
     // add a show code button right above
     var showCodeText = $('<span>' + (show ? 'Hide R code' : 'R code') + '</span>');
@@ -101,7 +99,8 @@ window.initializeCodeFolding = function(show) {
         .attr('data-target', '#' + id)
         .attr('aria-expanded', show)
         .attr('aria-controls', id);
-        
+    
+    // change the background color of the button        
     showCodeButton.css('background-color','#0000ff');
     
     var buttonRow = $('<div class="row"></div>');
